f349f502bd251ac185ff34e22a56703653e5948d
annabrisland <84166964+annabrisland@users.noreply.github.com>
2024-01-17 13:04:44 -0800

    Reorganises and routes pages

---
+++ src/components/pages/Play.jsx
@@ -0,0 +1,11 @@
+import MainBoard from "../MainBoard"
+
+const Play = () => {
+    return (
+        <div style={{ position: "relative", height: "auto" }}>
+            <MainBoard />
+        </div>
+    )
+}
+
+export default Play;
