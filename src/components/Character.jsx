479904c808556883191344a731b432a09d0e6f3e
Janeks111 <90975618+Janeks111@users.noreply.github.com>
2024-01-21 21:42:45 +0000

    edited character file

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,14 +1,25 @@
-import { useState, useEffect } from 'react'
-import { animate, motion } from "framer-motion"
+import { useState, useEffect } from "react";
+import { animate, motion } from "framer-motion";
 
-function Character({top, left, visibility}){
-
-    return (<motion.div 
-
-                animate={{ x:left, y:top}}
-                transition={{duration: 1}}
-                style={{ position: "absolute", top: 20, left: 30, width: 32, height: 80, backgroundColor: "blue", border: "3px solid black", visibility: visibility }} 
-                    > </motion.div>)
+function Character({ top, left, visibility }) {
+  return (
+    <motion.div
+      animate={{ x: left, y: top }}
+      transition={{ duration: 1 }}
+      style={{
+        position: "absolute",
+        top: 20,
+        left: 30,
+        width: 32,
+        height: 80,
+        backgroundColor: "blue",
+        border: "3px solid black",
+        visibility: visibility,
+      }}
+    >
+      {" "}
+    </motion.div>
+  );
 }
 
 export default Character;
